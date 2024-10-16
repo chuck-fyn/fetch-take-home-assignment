9bb6ae16bce2de60b1a9fa9f20b4beaea7a38004
Charlie Prutting <cprutting@gmail.com>
2024-10-15 20:38:10 -0700

    Initial Commit

---
+++ fetch-take-home-assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fetch-take-home-assignment
+//
+//  Created by Charles Prutting on 10/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
